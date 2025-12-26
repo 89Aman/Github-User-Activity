f79768bf860dfbf9b0ab2405cfdffe48adeea6e0
Aman Sharma <shasarita23@gmail.com>
2025-12-26 09:49:47 +0000

    Done coding

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+
+def fetch(uname):
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    url = f"https://api.github.com/users/{uname}/events/public"
+
+    try:
+        resp = requests.get(url, headers=headers, timeout=10)
+        resp.raise_for_status()
+        return resp.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
+
+
+map_eve = {
+    "CreateEvent": "Created repository",
+    "PushEvent": "Pushed commits to",
+    "WatchEvent": "Starred",
+    "IssueCommentEvent": "Commented on issue in",
+    "PullRequestEvent": "Closed pull request in"
+}
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <username>")
+        sys.exit(1)
+
+    uname = sys.argv[1]
+    events = fetch(uname)
+
+    for e in events:
+        t = e.get("type")
+        if t in map_eve:
+            print(f"- {map_eve[t]} {e.get('repo', {}).get('name')}")
